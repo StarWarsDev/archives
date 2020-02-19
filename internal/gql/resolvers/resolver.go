195b2620ce87ee652dae174c0bb80ac1b3d1d333
Stephen Good <stephen.good@umusic.com>
2020-02-18 19:21:43 -0800

    Added command cards for blank query. Revisit to reduce calls for data

--- internal/gql/resolvers/resolver.go
+++ internal/gql/resolvers/resolver.go
@@ -2,6 +2,8 @@ package resolvers
 
 import (
 	"context"
+	"github.com/StarWarsDev/archives/internal/transform"
+	legionhq "github.com/StarWarsDev/go-legion-hq"
 
 	"github.com/StarWarsDev/archives/internal/gql"
 	"github.com/StarWarsDev/archives/internal/gql/models"
@@ -21,23 +23,54 @@ func (r *queryResolver) Command(ctx context.Context, id string) (*models.Command
 	panic("not implemented")
 }
 func (r *queryResolver) Commands(ctx context.Context, query string) ([]*models.Command, error) {
-	panic("not implemented")
+	if query == "" {
+		data, err := legionhq.GetData()
+		if err != nil {
+			return nil, err
+		}
+
+		var commands []*models.Command
+
+		for _, card := range data.CommandCards() {
+			command := transform.CardToCommand(card)
+			commands = append(commands, &command)
+		}
+
+		return commands, nil
+	} else {
+		panic("query not implemented")
+	}
 }
 func (r *queryResolver) Keyword(ctx context.Context, name string) (*models.Keyword, error) {
 	panic("not implemented")
 }
 func (r *queryResolver) Keywords(ctx context.Context, query string) ([]*models.Keyword, error) {
-	panic("not implemented")
+	if query == "" {
+		// get everything
+		panic("not implemented")
+	} else {
+		panic("query not implemented")
+	}
 }
 func (r *queryResolver) Unit(ctx context.Context, id string) (*models.Unit, error) {
 	panic("not implemented")
 }
 func (r *queryResolver) Units(ctx context.Context, query string) ([]*models.Unit, error) {
-	panic("not implemented")
+	if query == "" {
+		// get everything
+		panic("not implemented")
+	} else {
+		panic("query not implemented")
+	}
 }
 func (r *queryResolver) Upgrade(ctx context.Context, id string) (*models.Upgrade, error) {
 	panic("not implemented")
 }
 func (r *queryResolver) Upgrades(ctx context.Context, query string) ([]*models.Upgrade, error) {
-	panic("not implemented")
+	if query == "" {
+		// get everything
+		panic("not implemented")
+	} else {
+		panic("query not implemented")
+	}
 }
